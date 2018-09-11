c5e46e07f4abbde414d646111cef5689b67f77da
kor-solidarity <confed00@naver.com>
2018-09-11 17:52:06 +0900

    임시 시험구동 용도.

--- app/article/models.py
+++ app/article/models.py
@@ -1,9 +1,31 @@
 from app import db
+from datetime import *
 
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     author = db.Column(db.String(50))
-    date = db.TIMESTAMP()
+    date = db.Column(db.TIMESTAMP)
     views = db.Column(db.Integer)
+    ip = db.Column(db.String(20))
+    # 무엇의 답글? 원글이면 아이디와 동일, 특정 글의 답글이면 그거 써져야함.
+    gnum = db.Column(db.Integer, nullable=True)
+    # 몇번째 답글?
+    onum = db.Column(db.Integer)
+    # 들여쓰기.
+    nested = db.Column(db.Integer)
+
+    def __init__(self, title, author, ip, gnum=None, onum=0, nested=0):
+        self.title = title
+        self.author = author
+        self.date = datetime.utcnow() + timedelta(hours=9)
+        self.views = 0
+        self.ip = ip
+        if gnum:
+            self.gnum = gnum
+        self.onum = onum
+        self.nested = nested
+
+    def __repr__(self):
+        return 'article {}'.format(self.title)
---
+++ app/test.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Set the path
+import os, sys
+sys.path.append(os.path.abspath
+                (os.path.join(os.path.dirname(__file__), '..')))
+app = Flask(__name__)
+app.config.from_object('app.settings')
+db = SQLAlchemy(app)
+class Aaa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    a = db.Column(db.Integer)
+
+
+@app.route('/thread/<int:page_num>')
+def thread(page_num):
+    threads = Aaa.query.paginate(per_page=20, page=page_num, error_out=True)
+
+    return render_template('thr.html', threads=threads)
+
+if __name__ == '__main__':
+    app.run(debug=True)
